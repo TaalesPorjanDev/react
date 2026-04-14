e74e91efc4fabe7333349705ae6500547f91e15a
Tales Porjan <taales.porjandev@gmail.com>
2026-04-14 08:17:42 -0300

    feat: implementação do useRef

--- curso-base-de-react/src/components/InputAdd.tsx
+++ curso-base-de-react/src/components/InputAdd.tsx
@@ -1,22 +1,35 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 interface IInputAddProps {
   onAdd(value: string): void;
 }
 
 export const InputAdd = (props: IInputAddProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const countRef = useRef(0)
+
+
   const [value, setValue] = useState('');
 
   const handleAdd = () => {
     props.onAdd(value);
     setValue('');
+    inputRef.current?.focus();
   };
 
   return (
     <div>
-      <input value={value} onChange={(event) => setValue(event.target.value)} />
+      <input 
+        value={value}
+        ref={inputRef} 
+        onChange={(event) => setValue(event.target.value)} 
+      />
 
-      <button onClick={handleAdd}>Adicionar</button>
+      {countRef.current}
+
+      <button onClick={handleAdd}>
+        Adicionar
+      </button>
     </div>
   );
 };
